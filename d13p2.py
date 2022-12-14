18707a68f0e707995e9678523d31781fd185c61b
Robert Hagen <hagen.robert@fronius.com>
2022-12-14 22:37:59 +0100

    day 13: knights of the dinner table

---
+++ d13p2.py
@@ -0,0 +1,30 @@
+import re
+
+persons = set()
+L = {}
+for line in  [line.strip() for line in open('d13.in')]:
+    p1, op, amt, p2 = re.match(r'(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\.', line).groups()
+    amt = int(amt)
+    L[(p1, p2)] = amt if op == 'gain' else -amt
+    persons.add(p1)
+    persons.add(p2)
+
+for p in persons:
+    L[(p, 'Me')] = 0
+    L[('Me', p)] = 0
+persons.add('Me')
+
+A = []
+def permutate(seats=[]):
+    if len(seats) == len(persons):
+        h = 0
+        for p1, p2 in zip(seats, seats[1:]):
+            h += L[(p1, p2)] + L[(p2, p1)]
+        h += L[(seats[0], seats[len(seats)-1])] + L[(seats[len(seats)-1], seats[0])]
+        A.append((h, seats))
+    for p in persons:
+        if p not in seats:
+            permutate(seats + [p])
+
+permutate([])
+print(max(A, key=lambda t: t[0])) 
