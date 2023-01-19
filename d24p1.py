481fbeb00b0c10c64121fe229ae7c1e4e8496249
Robert Hagen <hagen.robert@fronius.com>
2023-01-19 18:11:02 +0100

    day 24: it hangs in the balance

---
+++ d24p1.py
@@ -0,0 +1,14 @@
+from functools import reduce
+from itertools import combinations
+from operator import mul
+
+packages = [int(x.strip()) for x in open('d24.in').readlines()]
+weight = sum(packages) // 3
+
+for i in range(len(packages)):
+    qes = [reduce(mul, c)
+           for c in combinations(packages, i)
+           if sum(c) == weight]
+    if qes:
+        print(min(qes))
+        break
